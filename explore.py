5099bf69b0412af33cce73d7027b89ed213c3bb2
Gabriel Pereira <berkpereira@gmail.com>
2022-11-16 14:20:49 +0000

    added environment exploration .py file

--- explore.py
+++ explore.py
@@ -1,20 +1,14 @@
 import gym
 import gym_examples
-env = gym.make('gym_examples/GridWorld-v0', size=10, render_mode='human')
+env = gym.make('gym_examples/GridWorld-v0', size=4, render_mode='human')
 env.action_space.seed(42)
 
 observation, info = env.reset(seed=42)
 
-"""
 while True:
     observation, reward, terminated, truncated, info = env.step(env.action_space.sample())
-    print(env.action_space)
 
     if terminated or truncated:
-        observation, info = env.reset()
+        env.reset()
 
-"""
-
-
-observation, reward, terminated, truncated, info = env.step(env.action_space.sample())
 env.close()
