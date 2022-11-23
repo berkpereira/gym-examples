885c0f8622c5bc0f18ed3276e0a34f00887b49df
Gabriel Pereira <berkpereira@gmail.com>
2022-11-23 12:29:39 +0000

    initialised policy and policy evaluation functions

--- explore.py
+++ explore.py
@@ -1,14 +1,32 @@
 import gym
 import gym_examples
-env = gym.make('gym_examples/GridWorld-v0', size=4, render_mode='human')
-env.action_space.seed(42)
 
-observation, info = env.reset(seed=42)
+def grid_env_constructor(grid_size):
+    env = gym.make('gym_examples/GridWorld-v0', size=grid_size, render_mode='human')
+    env.action_space.seed(42)
+    return env
 
-while True:
-    observation, reward, terminated, truncated, info = env.step(env.action_space.sample())
 
-    if terminated or truncated:
-        env.reset()
+env = grid_env_constructor(4)
+# in the below, do env.rest(seed=42) to get repeated initialisations
+observation, info = env.reset()
 
-env.close()
+def test_policy(env):
+    #return env.action_space.sample()
+    return 1
+
+def policy_evaluation(policy):
+    
+    
+    while True:
+        action = policy(env)
+        observation, reward, terminated, truncated, info = env.step(action)
+        print(f'action: {action}    state: {observation["agent"]}    target: {observation["target"]}')
+
+        if terminated or truncated:
+            env.reset()
+
+    #env.close()
+
+
+policy_evaluation(test_policy)
